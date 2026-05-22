921b026431b848d859d41570c80164ac4c4b881c
vitoriazanchet <vitoria.zanchet@hotmail.com>
2026-05-22 12:38:24 -0300

    Configuração do swagger

---
+++ src/main/java/org/serratec/clinica/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package org.serratec.clinica.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API Clínica Médica")
+                        .version("1.0")
+                        .description("Sistema de Gestão de Consultas e Prontuários"));
+    }
+}
